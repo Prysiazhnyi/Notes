735b945234e8a00236ae2e5e48b12c2d7ce9d0f1
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-20 19:25:02 +0200

    Initial Commit

---
+++ Lessons-74-Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lessons-74-Notes
+//
+//  Created by Serhii Prysiazhnyi on 20.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
