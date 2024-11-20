7e0b5c404681e0ac0cd6c40f73fec9e994e3bb05
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-20 21:15:46 +0200

    start

---
+++ Lessons-74-Notes/DetailTextViewController.swift
@@ -0,0 +1,72 @@
+//
+//  DetailTextViewController.swift
+//  Lessons-74-Notes
+//
+//  Created by Serhii Prysiazhnyi on 20.11.2024.
+//
+
+import UIKit
+
+// Протокол делегата для передачи данных
+protocol DetailTextViewControllerDelegate: AnyObject {
+    func didSaveNote(_ note: String, at index: Int?)
+}
+
+class DetailTextViewController: UIViewController {
+    
+    @IBOutlet var detailText: UITextView!
+    
+    var selectNote = String()   // Текущая заметка
+    var noteIndex: Int?        // Индекс заметки (если редактируем существующую)
+    
+    weak var delegate: DetailTextViewControllerDelegate?  // Делегат
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Отображаем переданный текст заметки
+        detailText.text = selectNote
+        
+        // для отображения клавиатуры
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(saveNote))
+    }
+    
+    // Удаляем наблюдателей, чтобы избежать утечек памяти
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            detailText.contentInset = .zero
+        } else {
+            detailText.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        detailText.scrollIndicatorInsets = detailText.contentInset
+        
+        let selectedRange = detailText.selectedRange
+        detailText.scrollRangeToVisible(selectedRange)
+    }
+    
+    @objc func saveNote() {
+           // Уведомляем делегата о сохранении заметки
+           delegate?.didSaveNote(detailText.text, at: noteIndex)
+           
+           // Возвращаемся назад
+           navigationController?.popViewController(animated: true)
+        
+        print("Detail save -- \(detailText.text)")
+       }
+    
+    
+}
--- Lessons-74-Notes/ViewController.swift
+++ Lessons-74-Notes/ViewController.swift
@@ -7,13 +7,90 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController, DetailTextViewControllerDelegate {
+    
+    var notes = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Нотатки"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteNote))
+        
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Note", for: indexPath)
+        
+        // Основной текст
+        cell.textLabel?.text = notes[indexPath.row]
+        
+        // Подзаголовок для каждой строки
+        // cell.detailTextLabel?.text = "Кол-во просмотров: \(pictures[indexPath.row].viewCount)"  // Пример текста подзаголовка
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let note = notes[indexPath.item]
+        
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailTextViewController {
+            
+            vc.selectNote = note
+            //vc.delegate = self  // Устанавливаем делегата для обновлений
+            print(note)
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    @objc func addNote() {
+        print("Add new note")
+        
+        let ac = UIAlertController(title: "Введіть назву", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let noteAction = UIAlertAction(title: "Зберегти", style: .default) { [weak self, weak ac] action in
+            guard let newNote = ac?.textFields?[0].text, !newNote.isEmpty else { return }
+            
+            // Добавляем новую заметку в массив
+            self?.notes.append(newNote)
+            
+            // Обновляем таблицу
+            self?.tableView.reloadData()
+        }
+        ac.addAction(noteAction)
+        present(ac, animated: true)
+    }
+    
+    
+    @objc func deleteNote() {
+        if let indexPath = tableView.indexPathForSelectedRow {
+            notes.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    // MARK: - Реализация делегата
+    func didSaveNote(_ note: String, at index: Int?) {
+        
+        print("Tab view save - \(note)")
+        
+        if let index = index {
+            // Обновляем существующую заметку
+            notes[index] = note
+        } else {
+            // Добавляем новую заметку
+            notes.append(note)
+        }
+        
+        // Обновляем таблицу
+        tableView.reloadData()
+    }
 }
 
